9e1d028ef0d5572f533abd7ecd6864822b87079f
Leo Pold <iamdreamer@yandex.ru>
2024-06-06 11:42:21 +0300

    beta run

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(name, link, wishFunction) {
+  const cardSample = cardTemplate.cloneNode(true);
+  const deleteCardButton = cardSample.querySelector('.card__delete-button');
+  cardSample.querySelector('.card__title').textContent = name;
+  cardSample.querySelector('.card__image').src = link;
+  cardContainer.append(cardSample); 
+  deleteCardButton.addEventListener('click', wishFunction);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  evt.target.parentElement.remove();
+}
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => addCard(item.name, item.link, deleteCard));
